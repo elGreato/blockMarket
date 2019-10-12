ccdd99c6d4df73099ce470d007e361db60f0844f
Hyp-nos <ali.habbabeh@hotmail.com>
2019-10-12 07:01:24 +0200

    refactoring and splitting navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3';
-import { Web3Provider } from 'react';
+import Navbar from './Navbar'
+//we need to import the contract that we created to interact with it from the front end
+import Marketplace from '../abis/Marketplace.json'
 
 class App extends Component {
   // this runs everytime the compnent gets created (life cycle component)
@@ -30,17 +32,47 @@ class App extends Component {
     const web3 = window.web3
     //load account
     const acc = await web3.eth.getAccounts();
-
+    console.log(acc)
     //now we use the state component of react to save the state of our account      
     this.setState({ account: acc[0] })
+
+    //load the main contract json where we can access the abi and address of the contract
+    //both ABI and Address are needed to instantiate the smart contract
+    //console.log(Marketplace.networks[5777].address)
+    const abi = Marketplace.abi
+
+    //hardcode network id for ganache
+    //const address = Marketplace.networks[5777].address
+
+    //better way to do it
+    const networkId= await web3.eth.net.getId()
+    const networkData = Marketplace.networks[networkId]
+
+    //check if network has an id
+    if (networkData){
+      const address = networkData.address
+      const marketplace = web3.eth.Contract(abi, address)
+      console.log(marketplace)
+
+    }else {
+      window.alert('your contract is not deployed to the current network')
+    }
+    
+
+    
+
   }
 
-  //constructor of React
 
+  //constructor of React
+  //props allows to pass down variables to subcomponents e.g. Navbar
   constructor(props) {
     super(props)
     this.state = {
-      account: ''
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
     }
   }
 
@@ -48,19 +80,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ali Tester
-          </a>
-          <p>
-            {this.state.account}
-          </p>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -72,17 +92,17 @@ class App extends Component {
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>elGreato Dapps</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
                 <a
                   className="App-link"
-                  href=""
+                  href="https://elgreato.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  LEARN BLOCKCHAIN <u><b>with elGreato! </b></u>
                 </a>
               </div>
             </main>
